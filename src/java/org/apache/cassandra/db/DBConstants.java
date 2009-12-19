83808cd5ad26723fea97f58ee541bceccb7b4073
Stu Hood <stuhood@stu-laptop.(none)>
2009-12-19 15:46:15 -0500

    Fix the size calculation for Column.

--- src/java/org/apache/cassandra/db/DBConstants.java
+++ src/java/org/apache/cassandra/db/DBConstants.java
@@ -21,6 +21,7 @@ package org.apache.cassandra.db;
 final class DBConstants
 {
 	public static final int boolSize_ = 1;
+	public static final int shortSize_ = 2;
 	public static final int intSize_ = 4;
 	public static final int longSize_ = 8;
 	public static final int tsSize_ = 8;
